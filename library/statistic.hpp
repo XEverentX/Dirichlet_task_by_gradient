ed6dc16c4889f15cdcc7f2c72fa47296d4a0f2a0
Alexander Lebedev <inok.alex@gmail.com>
2020-06-02 02:30:36 +0300

    [Feature] Add statistic and reference getter as member of the statistic

--- library/statistic.hpp
+++ library/statistic.hpp
@@ -3,6 +3,38 @@
 #ifndef LIBRARY_STATISTIC_HPP_
 #define LIBRARY_STATISTIC_HPP_
 
+#include <vector>
+#include <string>
 
+class Statistic final
+{
+ public:
+    Statistic() = default;
+
+    Statistic(const Statistic &other) = default;
+    
+    Statistic(Statistic &&other) = default;
+
+    ~Statistic() = default;
+
+    std::string getReference() const;
+
+ private:
+    int    n;
+    int    m;
+    int    maxCount;
+    int    count;
+    double minEps;
+    double eps;
+    double discrepancyNorm;
+    double inaccuracy;
+    double initialDiscrepancyNorm;
+    double maxDiscrepancyX;
+    double maxDiscrepancyY;
+
+    std::vector<std::vector<double>> realValue;
+    std::vector<std::vector<double>> solvedValue;
+    std::vector<std::vector<double>> diffValue;
+};
 
 #endif  // LIBRARY_STATISTIC_HPP_
