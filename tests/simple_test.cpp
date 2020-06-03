a9511607cb1957c876d514445466a4b7bd97c770
Alexander Lebedev <inok.alex@gmail.com>
2020-06-03 17:24:58 +0300

    [Test] Add test for scalarMul

--- tests/simple_test.cpp
+++ tests/simple_test.cpp
@@ -2,18 +2,28 @@
 
 #include <gtest/gtest.h>
 
-class CanTest : public ::testing::Test {
+#include <task.hpp>
+#include <statistic.hpp>
+#include <util.hpp>
+
+#include <vector>
+
+class simpleTest : public ::testing::Test {
  protected:
     double epsilon = 0.0000001;
 };
 
-TEST_F(CanTest, CanTest) {
+TEST_F(simpleTest, scalarMul) {
     // Arrange
-    int x = 5;
+    std::vector<std::vector<double>> v1 = {{0., 1.}, {2., 0.}};
+    std::vector<std::vector<double>> v2 = {{7.5, 3.}, {4.2, 0.}};
+    double expected = 11.4;
 
     // Act
-    int y = 5;
+    double result1 = util::scalarMul(v1, v2);
+    double result2 = util::scalarMul(v2, v1);
 
     // Assert
-    ASSERT_EQ(x, y);
+    ASSERT_EQ(expected, result1);
+    ASSERT_EQ(expected, result2);
 }
