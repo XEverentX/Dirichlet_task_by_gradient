e804ad42f800a42e9b73384680ed9c4ae34cab99
Alexander Lebedev <inok.alex@gmail.com>
2020-06-04 10:11:43 +0300

    [Test] Removed scalarMul test due to the new fix of the slove

--- tests/simple_test.cpp
+++ tests/simple_test.cpp
@@ -14,21 +14,6 @@ class simpleTest : public ::testing::Test {
     double epsilon = 0.0001;
 };
 
-TEST_F(simpleTest, scalarMul) {
-    // Arrange
-    std::vector<std::vector<double>> v1 = {{0., 1.}, {2., 0.}};
-    std::vector<std::vector<double>> v2 = {{7.5, 3.}, {4.2, 0.}};
-    double expected = 11.4;
-
-    // Act
-    double result1 = util::scalarMul(v1, v2);
-    double result2 = util::scalarMul(v2, v1);
-
-    // Assert
-    ASSERT_EQ(expected, result1);
-    ASSERT_EQ(expected, result2);
-}
-
 TEST_F(simpleTest, fIsValid) {
     // Arrange
     double expected = -6.18309;
